912fd2e37d93120bc60b9f4b233fb2eddd160279
Miguel Rodriguez <7migro11@gmail.com>
2022-07-12 23:09:02 -0700

    added new website

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+alert('Hello there!');
+
+let yourName = prompt('My name is Miguel, what is yours'); console.log(yourName);
+
+let questionone= prompt(yourName + ' I have a couple yes and no question for you. Would you like to answer?').toLocaleLowerCase();
+console.log(questionone);
+
+if (questionone === 'yes' || questionone === 'y') {
+  alert('Awesome let\s proceed.');
+} else if (questionone === 'no' || questionone === 'n') {
+  alert('Well that sucks on to the next question.');
+}
+
+let questiontwo= prompt('Does the developer Miguel like Science');
+console.log(questionone);
+
+if (questionone === 'yes' || questionone === 'y') {
+  alert('You are correct.');
+} else if (questionone === 'no' || questionone === 'n') {
+  alert('He does.');
+}
+
+let question3 = prompt('Does Miguel like the Sun?');
+console.log(question3);
+
+if (question3 === 'yes' || question3 === 'y') {
+  alert('You are correct');
+} else if (question3 === 'no' || question3 === 'n') {
+  alert('He does');
+}
+
+let question4 = prompt('Do I like chocolate?');
+console.log(question3);
+
+if (question4 === 'yes' || question4 === 'y') {
+  alert('Correct.');
+} else if (question4  === 'no' || question4  === 'n') {
+  alert('wrong! Next Question');
+}
+
+let question5 = prompt('Did you like this quiz');
+
+if (question5 === 'yes' || question5 === 'y') {
+  alert('Thanks much appreciated');
+} else if (question5 === 'no' || question5 === 'n') {
+  alert('Well I\'ll try better next time');
+}
+
+alert('Thank\'s for your time ' + yourName +'!');
+console.log(yourName);
+
+
