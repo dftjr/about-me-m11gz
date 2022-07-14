f9b2e232290eb726bb558f335d710b77fc95471c
Miguel Rodriguez <7migro11@gmail.com>
2022-07-13 22:30:04 -0700

    New Addition to Website

--- js/app.js
+++ js/app.js
@@ -1,53 +1,63 @@
 'use strict';
-alert('Hello there!');
+// alert('Hello there!');
 
-let yourName = prompt('My name is Miguel, what is yours'); console.log(yourName);
+// let yourName = prompt('My name is Miguel, what is yours'); console.log(yourName);
 
-let questionone= prompt(yourName + ' I have a couple yes and no question for you. Would you like to answer?').toLocaleLowerCase();
-console.log(questionone);
+// let questionone= prompt(yourName + ' I have a couple yes and no question for you. Would you like to answer?').toLocaleLowerCase();
+// console.log(questionone);
 
-if (questionone === 'yes' || questionone === 'y') {
-  alert('Awesome let\s proceed.');
-} else if (questionone === 'no' || questionone === 'n') {
-  alert('Well that sucks on to the next question.');
-}
+// if (questionone === 'yes' || questionone === 'y') {
+//   alert('Awesome let\s proceed.');
+// } else if (questionone === 'no' || questionone === 'n') {
+//   alert('Well that sucks on to the next question.');
+// }
 
-let questiontwo= prompt('Does the developer Miguel like Science');
-console.log(questionone);
+// let questiontwo= prompt('Does the developer Miguel like Science');
+// console.log(questionone);
 
-if (questionone === 'yes' || questionone === 'y') {
-  alert('You are correct.');
-} else if (questionone === 'no' || questionone === 'n') {
-  alert('He does.');
-}
+// if (questionone === 'yes' || questionone === 'y') {
+//   alert('You are correct.');
+// } else if (questionone === 'no' || questionone === 'n') {
+//   alert('He does.');
+// }
 
-let question3 = prompt('Does Miguel like the Sun?');
-console.log(question3);
+// let question3 = prompt('Does Miguel like the Sun?');
+// console.log(question3);
 
-if (question3 === 'yes' || question3 === 'y') {
+// if (question3 === 'yes' || question3 === 'y') {
+//   alert('You are correct');
+// } else if (question3 === 'no' || question3 === 'n') {
+//   alert('He does');
+// }
+
+// let question4 = prompt('Do I like chocolate?');
+// console.log(question3);
+
+// if (question4 === 'yes' || question4 === 'y') {
+//   alert('Correct.');
+// } else if (question4  === 'no' || question4  === 'n') {
+//   alert('wrong! Next Question');
+// }
+
+// let question5 = prompt('Did you like this quiz');
+
+// if (question5 === 'yes' || question5 === 'y') {
+//   alert('Thanks much appreciated');
+// } else if (question5 === 'no' || question5 === 'n') {
+//   alert('Well I\'ll try better next time');
+// }
+
+// alert("You have 4 guesses. Pick a number 1-10");
+
+let guess = 4;
+
+for(let i = guess; i>=0; i--) {
+let kanye = prompt('Let\'s play a game. Pick a number from 1-10');
+if (kanye === '4') {
   alert('You are correct');
-} else if (question3 === 'no' || question3 === 'n') {
-  alert('He does');
+}
+break; 
 }
 
-let question4 = prompt('Do I like chocolate?');
-console.log(question3);
-
-if (question4 === 'yes' || question4 === 'y') {
-  alert('Correct.');
-} else if (question4  === 'no' || question4  === 'n') {
-  alert('wrong! Next Question');
-}
-
-let question5 = prompt('Did you like this quiz');
-
-if (question5 === 'yes' || question5 === 'y') {
-  alert('Thanks much appreciated');
-} else if (question5 === 'no' || question5 === 'n') {
-  alert('Well I\'ll try better next time');
-}
-
-alert('Thank\'s for your time ' + yourName +'!');
-console.log(yourName);
-
-
+// alert('Thank\'s for your time ' + yourName +'!');
+// console.log(yourName);
